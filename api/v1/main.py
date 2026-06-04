0f54d45a59fd8cff46a3cf5bd4377c865ed35683
Marcin Kasperski <marcin.kspr@gmail.com>
2026-06-04 14:52:58 +0200

    added Ollama, with qwen3:4b

--- api/v1/main.py
+++ api/v1/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+import requests
 
 from v1.schemas import MessageRequest
+from v1.schemas import ClassificationRequest
 from v1.tools import send_email
 
 app = FastAPI(
@@ -25,4 +27,40 @@ def route_message(request: MessageRequest):
 
     return {
         "status": "sent"
+    }
+
+@app.post("/api/v1/classify")
+def classify(request: ClassificationRequest):
+
+    prompt = f"""
+Wybierz jeden dział dla wiadomości.
+
+Dostępne działy:
+
+- human-resources@example.com
+- help-desk@example.com
+- it@example.com
+- kadry@example.com
+- other@example.com
+
+Wiadomość:
+
+{request.message}
+
+Odpowiedz wyłącznie adresem email działu.
+"""
+
+    response = requests.post(
+        "http://ollama:11434/api/generate",
+        json={
+            "model": "qwen3:4b",
+            "prompt": prompt,
+            "stream": False
+        }
+    )
+
+    result = response.json()
+
+    return {
+        "department": result["response"]
     }
--- api/v1/schemas.py
+++ api/v1/schemas.py
@@ -4,3 +4,6 @@ from pydantic import BaseModel, EmailStr
 class MessageRequest(BaseModel):
     email: EmailStr
     message: str
+
+class ClassificationRequest(BaseModel):
+    message: str
