c31e9ee73cd621d3731b3b14e47d462afb9df065
Marcin Kasperski <marcin.kspr@gmail.com>
2026-06-05 10:49:52 +0200

    change email subject

--- api/v1/tools.py
+++ api/v1/tools.py
@@ -5,7 +5,7 @@ from email.message import EmailMessage
 def send_email(to_email: str, sender_email: str, message_text: str):
     msg = EmailMessage()
 
-    msg["Subject"] = "AI Router Test"
+    msg["Subject"] = "Automatically rerouted email"
     msg["From"] = "router@example.com"
     msg["To"] = to_email
     msg["Reply-To"] = sender_email
